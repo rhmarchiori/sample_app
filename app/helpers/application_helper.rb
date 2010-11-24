336507c59d1d75b12768a892f981828a339238c4
rhmarchiori <rhsmarchiori@gmail.com>
2010-11-24 01:35:58 -0200

    Completed Layout Structure

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   
+  # Return a title on per-page basis.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
