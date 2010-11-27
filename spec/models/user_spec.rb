7bd2051e858d08ae5d5bb19b9c18ec97381cf7dd
rhmarchiori <rhsmarchiori@gmail.com>
2010-11-27 13:43:46 -0200

    Added password and encrypted_password attributes to the User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,12 @@ require 'spec_helper'
 describe User do
   
   before(:each) do
-    @attr = {:name=>"Example Use", :email=>"user@example.com"} ;
+    @attr = {
+      :name=>"Example Use", 
+      :email=>"user@example.com", 
+      :password => "foobar",
+      :password_confirmation => "foobar" 
+    }
   end
   
   it "should create a new instance given a valid attribute" do
@@ -54,4 +59,55 @@ describe User do
     user_with_duplicated_email = User.new(@attr)
     user_with_duplicated_email.should_not be_valid
   end
+  
+  describe "passwords" do
+    
+    before(:each) do
+      @user = User.new(@attr)
+    end
+    
+    it "should have a password attribute" do
+      @user.should respond_to(:password)
+    end
+    
+    it "should have a password confirmation attribute" do
+      @user.should respond_to(:password_confirmation)
+    end
+  end
+  
+  describe "passowrd validations" do
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => " ")).
+          should_not be_valid
+    end
+    
+    it "should require a matching password confirmation" do
+      User.new(@attr.merge(:password_confirmation=>"invalid")).
+          should_not be_valid
+    end
+    
+    it "should reject short passwords" do
+      short_password = "a" * 5
+      hash = @attr.merge(:password => short_password, :password_confirmation => short_password)
+      User.new(hash).should_not be_valid
+    end
+    
+    it "should reject short passwords" do
+      long_password = "a" * 50
+      hash = @attr.merge(:password => long_password, :password_confirmation => long_password)
+      User.new(hash).should_not be_valid
+    end
+  end
+  
+  describe "password encryption" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)  
+    end
+  end
+    
 end
